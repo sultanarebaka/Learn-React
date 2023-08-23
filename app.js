64b784b97160e2d10696a0fba99f108411691578
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-23 12:19:53 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-function app ()
+function App ()
+{
+
+    return (
+        <div className='container'>
+            <h1>Hello From React</h1>
+        </div>
+    )
+    
+}
+
+export default App
